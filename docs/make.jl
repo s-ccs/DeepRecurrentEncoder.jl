f1a71d27a2f35a76bfae6f6234bd3715062321d1
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2024-05-31 13:12:14 +0000

    recover the make and fix bug

---
+++ docs/make.jl
@@ -0,0 +1,37 @@
+using DeepRecurrentEncoder
+using Documenter
+using Literate
+DocMeta.setdocmeta!(DeepRecurrentEncoder, :DocTestSetup, :(using DeepRecurrentEncoder); recursive=true)
+
+
+
+GENERATED = joinpath(@__DIR__, "src", "generated")
+SOURCE = joinpath(@__DIR__, "literate")
+
+for subfolder ∈ ["explanations", "howto", "tutorials", "reference"]
+    local SOURCE_FILES = Glob.glob(subfolder * "/*.jl", SOURCE)
+    #config=Dict(:repo_root_path=>"https://github.com/unfoldtoolbox/UnfoldSim")
+    foreach(fn -> Literate.markdown(fn, GENERATED * "/" * subfolder), SOURCE_FILES)
+
+end
+
+
+
+makedocs(;
+    modules=[DeepRecurrentEncoder],
+    authors="Benedikt V. Ehinger",
+    sitename="DeepRecurrentEncoder.jl",
+    format=Documenter.HTML(;
+        canonical="https://s-ccs.github.io/DeepRecurrentEncoder.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/s-ccs/DeepRecurrentEncoder.jl",
+    devbranch="main",
+)
