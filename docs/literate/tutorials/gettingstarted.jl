861592342774d69f094c26028a088fae40d79dda
shaolong <shaolongshi2021@gmail.com>
2024-06-16 17:55:04 +0200

    Initial commit

--- docs/literate/tutorials/gettingstarted.jl
+++ docs/literate/tutorials/gettingstarted.jl
@@ -1,11 +1,12 @@
 # Getting started
-
+    
 # # this is a markdown headline
 
 ## this is a code comment
 
-1 == 1
+1 == 1  
 
 
 # text will make a new code cell with new output_data
-bla = "blub"
+bla = "ramnath"
+
