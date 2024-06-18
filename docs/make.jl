b7d7c5d538877d8fba94e14b24b923b6decfa0b2
shaolong <shaolongshi2021@gmail.com>
2024-06-18 22:30:24 +0200

    documentation

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,6 @@ using Literate, Glob
 DocMeta.setdocmeta!(DeepRecurrentEncoder, :DocTestSetup, :(using DeepRecurrentEncoder); recursive=true)
 
 
-
 GENERATED = joinpath(@__DIR__, "src", "generated")
 SOURCE = joinpath(@__DIR__, "literate")
 
@@ -17,20 +16,38 @@ end
 
 
 
-makedocs(;
-    modules=[DeepRecurrentEncoder],
-    authors="Benedikt V. Ehinger",
-    sitename="DeepRecurrentEncoder.jl",
-    format=Documenter.HTML(;
-        canonical="https://s-ccs.github.io/DeepRecurrentEncoder.jl",
-        edit_link="main",
-        assets=String[],
+# makedocs(;
+#     modules=[DeepRecurrentEncoder],
+#     authors="Benedikt V. Ehinger",
+#     sitename="DeepRecurrentEncoder.jl",
+#     format=Documenter.HTML(;
+#         canonical="https://s-ccs.github.io/DeepRecurrentEncoder.jl",
+#         edit_link="main",
+#         assets=String[],
+#     ),
+#     pages=[
+#         "Home" => "index.md",
+#         "Getting Started" => "generated/tutorials/gettingstarted.md",
+#         "Team_report" => "generated/tutorials/Autoencoder_EEG_Meeting_Minutes.md"
+#     ],
+# )
+
+makedocs(
+     modules=[DeepRecurrentEncoder],
+     authors="Benedikt V. Ehinger",
+     sitename="DeepRecurrentEncoder.jl",
+     format=Documenter.HTML(;
+         canonical="https://s-ccs.github.io/DeepRecurrentEncoder.jl",
+         edit_link="main",
+         assets=String[],
     ),
-    pages=[
+    pages = Any[
         "Home" => "index.md",
-        "Getting Started" => "generated/tutorials/gettingstarted.md",
-        "Team_report" => "generated/tutorials/Autoencoder_EEG_Meeting_Minutes.md"
-    ],
+        "Tutorials" => [
+            "Autoencoder EEG Meeting Minutes" => "generated/tutorials/Autoencoder_EEG_Meeting_Minutes.md",
+            "Getting Started" => "generated/tutorials/gettingstarted.md"
+        ]
+    ]
 )
 
 deploydocs(;
